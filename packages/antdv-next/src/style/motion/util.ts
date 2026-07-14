09c9e99a0a7aea7e82e3da1199ea8327f69e3a47
aibayanyu20 <aibayanyu@qq.com>
2026-07-13 17:51:14 -0700

    fix(motion): disable before & after dom motion (#58685)

--- packages/antdv-next/src/style/motion/util.ts
+++ packages/antdv-next/src/style/motion/util.ts
@@ -3,8 +3,10 @@ import type { CSSObject } from '@antdv-next/cssinjs'
 export function genNoMotionStyle(): CSSObject {
   return {
     '@media (prefers-reduced-motion: reduce)': {
-      transition: 'none',
-      animation: 'none',
+      '&, &::before, &::after': {
+        transition: 'none',
+        animation: 'none',
+      },
     },
   }
 }
